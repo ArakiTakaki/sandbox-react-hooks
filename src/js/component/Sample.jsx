b0b353a7355c319b41a259a81f992d46e8332e96
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-03-21 01:29:05 +0900

    MyStore完成

--- src/js/component/Sample.jsx
+++ src/js/component/Sample.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Connect from '../store/connect';
+import * as Actions from '~/store/actions'
 
+@Connect
 class Sample extends React.Component {
+  constructor (props) {
+    super(props);
+    this.onIncrement = this.onIncrement.bind(this);
+  }
+
+  onIncrement() {
+    this.props.dispatch(Actions.increment());
+  }
+
   render() {
     console.log(this.props);
-    return <h1>test</h1>;
+    const { store, dispatch } = this.props;
+    return (
+      <div>
+        <div>{store.user.name}</div>
+        <div>{store.count}</div>
+        <div>
+          <input type="button" onClick={this.onIncrement} value="+"/>
+          <input type="button" onClick={()=>{dispatch(Actions.decrement())}} value="-"/>
+        </div>
+        <h1>test</h1>
+      </div>
+    );
   }
 }
 
@@ -13,5 +35,4 @@ Sample.propTypes = {
   children: PropTypes.any
 };
 
-export default Connect(Sample);
-
+export default Sample;
